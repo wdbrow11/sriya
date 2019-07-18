175157b5c11ed11ced97d9db426186adfcc88946
Will Brown <williambrown@williams-air.lan>
2019-07-17 23:30:50 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var shoutouts = [
+  "You are so helpful and you are easy to talk to as a peer, rather than an IA.",
+  "Your determination! I remember that one time we were both struggling to make my project work but we got through it!! Thanks for always helping me out",
+  "thanks for helping me whenever I'm stuck",
+  "How helpful you are. Not only do you constantly help us but you help us figure it out on our own as well so we can use that method the next time.",
+  "You're so sweet!! I love your smile and laugh, and you are soooo cute! Thanks for always helping me too.",
+  "how comfortable it is to ask for and receive help from you!",
+  "You are so easy to talk to and you are such great help when I'm stuck.",
+  "how helpful you are and how great your explanations are (don't underestimate yourself!). How you always have a funny story to tell and a smile ready.",
+  "that you are always here to help people.",
+  "How helpful you are and that you made us feel welcome from the start!",
+  "Your enthusiasm and kindness and eagerness to help.",
+  "How you check up on us and make us feel included!",
+  "how patient you are and how much you are always willing to help.",
+  "You always take so much time to make sure that everyone’s problems are solved and your warm spirit is so wonderful to be around."
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
